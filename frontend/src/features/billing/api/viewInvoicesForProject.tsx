4725c885de3a55109929646ca72c47e7f31b7e2e
AlexRemiNoel <sasha.f.noel@gmail.com>
2025-12-14 22:55:58 -0500

    updated billing panel

--- frontend/src/features/billing/api/viewInvoicesForProject.tsx
+++ frontend/src/features/billing/api/viewInvoicesForProject.tsx
@@ -1,9 +1,9 @@
 import { client } from "@shared/lib/client";
 import type { Invoice } from "@features/billing/types/Invoice";
 
-export default async function fetchInvoice(projectId: string): Promise<Invoice> {
+export default async function fetchInvoice(projectId: string): Promise<Invoice[]> {
   try {
-    const res = await client.get<Invoice>(`/api/v1/billing/${projectId}/invoices/`);
+    const res = await client.get<Invoice[]>(`/api/v1/billing/${projectId}/invoices/`);
     return res.data;
   } catch (error: any) {
     throw error;
